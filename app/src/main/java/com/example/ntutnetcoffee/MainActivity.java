fcdc50469244ea1c02e982f4943d5715dfb62745
Yeeming0622 <asdasd121232145@gmail.com>
2023-12-26 14:42:30 +0800

    modify onCreate

--- app/src/main/java/com/example/ntutnetcoffee/MainActivity.java
+++ app/src/main/java/com/example/ntutnetcoffee/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.ntutnetcoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mAirplane, mSnake, mOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAirplane = findViewById(R.id.Airplane);
+        mSnake = findViewById(R.id.Snake);
+        mOrder = findViewById(R.id.Order);
+
+        mAirplane.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mSnake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
