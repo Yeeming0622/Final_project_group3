c75a75f46f5d17177a0f8436c34166126e269a13
110360104 <jacky24588341@gmail.com>
2023-12-30 18:35:58 +0800

    snake project finish

--- app/src/main/java/com/example/ntutnetcoffee/MainActivity_snake_2.java
+++ app/src/main/java/com/example/ntutnetcoffee/MainActivity_snake_2.java
@@ -2,13 +2,61 @@ package com.example.ntutnetcoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity_snake_2 extends AppCompatActivity {
+public class MainActivity_snake_2 extends AppCompatActivity implements View.OnClickListener {
 
+    private SnakePanelView mSnakePanelView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_snake2);
+        Bundle bundle = getIntent().getExtras();
+        String tv_id2 = bundle.getString("id" );
+
+        mSnakePanelView = findViewById(R.id.snake_view);
+        findViewById(R.id.left_btn).setOnClickListener(this);
+        findViewById(R.id.right_btn).setOnClickListener(this);
+        findViewById(R.id.top_btn).setOnClickListener(this);
+        findViewById(R.id.bottom_btn).setOnClickListener(this);
+        findViewById(R.id.start_btn).setOnClickListener(this);
+        mSnakePanelView.reSet();
+
+        Button btn_score = (Button) findViewById(R.id.btn_score);
+        btn_score.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity_snake_2.this, MainActivity_snake_3.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("id",tv_id2);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.left_btn) {
+            mSnakePanelView.setSnakeDirection(GameType.LEFT);
+        } else if (v.getId() == R.id.right_btn) {
+            mSnakePanelView.setSnakeDirection(GameType.RIGHT);
+        } else if (v.getId() == R.id.top_btn) {
+            mSnakePanelView.setSnakeDirection(GameType.TOP);
+        } else if (v.getId() == R.id.bottom_btn) {
+            mSnakePanelView.setSnakeDirection(GameType.BOTTOM);
+        } else if (v.getId() == R.id.start_btn) {
+            mSnakePanelView.reStartGame();
+        }
     }
 }
---
+++ app/src/main/java/com/example/ntutnetcoffee/MainActivity_snake_3.java
@@ -0,0 +1,50 @@
+package com.example.ntutnetcoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity_snake_3 extends AppCompatActivity implements Listener{
+
+    @Override
+    public void send(String s) {
+        Log.e("tag: ", s);
+        TextView tv_score = findViewById(R.id.tv_score);
+        tv_score.setText(String.format("%s", s));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_snake3);
+
+        Bundle bundle = getIntent().getExtras();
+        String tv_id2 = bundle.getString("id" );
+        TextView tv_id3 = (TextView)findViewById(R.id.tv_id3);
+        tv_id3.setText(tv_id2);
+
+        SnakePanelView.Data data = new SnakePanelView.Data(this);
+        data.sends();
+
+        Button btn_re = findViewById(R.id.btn_re);
+
+        btn_re.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity_snake_3.this, MainActivity_snake_1.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        moveTaskToBack(true);
+    }
+}
